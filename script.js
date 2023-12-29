6af67fd7ddafe3b4018b4cbd09492335222636d8
Shivam Singh gautam <105829038+Shivu150199@users.noreply.github.com>
2023-12-29 18:18:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // write js code here if required
+const timer=document.querySelector('#timer')
+let day=new Date().getDate()
+let month=new Date().getMonth()+1
+let year=new Date().getFullYear()
+let hour=new Date().getHours()
+let minutes=new Date().getMinutes()
+let seconds=new Date().getSeconds();
+timer.innerText=`${day}/${month}/${year},${hour}:${minutes}:${seconds}`
